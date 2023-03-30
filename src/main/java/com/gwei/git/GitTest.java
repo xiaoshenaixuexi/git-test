3b54bb405f3a7a40f8de9d137f1a4669be547d90
xiaoshenaixuexi <1308798543@qq.com>
2023-03-30 11:47:45 +0800

    first commit

---
+++ src/main/java/com/gwei/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.gwei.git;
+
+/**
+ * @author 高伟
+ * @create 2023-03-30 11:46
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git! version 1.0");
+    }
+}
