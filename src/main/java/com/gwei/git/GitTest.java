025e1da3dcbc429047828fb9f6a51d49cb5d4edb
xiaoshenaixuexi <1308798543@qq.com>
2023-03-30 11:50:44 +0800

    hot-fix first commit

--- src/main/java/com/gwei/git/GitTest.java
+++ src/main/java/com/gwei/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("Hello Git! version 1.0");
         System.out.println("Hello Git! version 2.0");
         System.out.println("Hello Git! version 3.0");
+        System.out.println("hot-fix branch test!");
     }
 }
