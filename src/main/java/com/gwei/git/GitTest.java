2c74ed457fa0c3a151407b72636b53732a38b7b5
xiaoshenaixuexi <1308798543@qq.com>
2023-03-30 11:49:00 +0800

    third commit

--- src/main/java/com/gwei/git/GitTest.java
+++ src/main/java/com/gwei/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git! version 1.0");
         System.out.println("Hello Git! version 2.0");
+        System.out.println("Hello Git! version 3.0");
     }
 }
